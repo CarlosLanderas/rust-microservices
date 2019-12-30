103f6c2057f33abb8943ac83d9d49c2685ffb1be
Carlos Landeras <carlos.landeras@gmail.com>
2019-12-30 09:43:45 +0100

    Initial image-service project

---
+++ async/image-service/src/main.rs
@@ -0,0 +1,18 @@
+use std::path::Path;
+use std::fs;
+use hyper::Server;
+use hyper::service::service_fn;
+use futures::Future;
+
+fn main() {
+    let files = Path::new("./files");
+    fs::create_dir(files).ok();
+    let addr = ([127,0,0,1], 8080).into();
+    let builder = Server::bind(&addr);
+    let server = builder.serve(move || {
+        service_fn(move |req| microservice_handler(req, &files))
+    });
+
+    let server = server.map_err(drop);
+    hyper::rt::run(server);
+}
