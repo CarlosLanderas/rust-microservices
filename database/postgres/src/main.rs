54e34f154c0e9a91c69e1b1f3e36f78536971ac4
Carlos <carlos.landeras@gmail.com>
2019-12-30 16:21:58 +0100

    Add clap app configuration

--- database/postgres/src/main.rs
+++ database/postgres/src/main.rs
@@ -2,14 +2,44 @@ use clap::{
     crate_authors, crate_description, crate_name, crate_version, App, AppSettings, Arg, SubCommand,
 };
 use postgres::{Connection, Error, TlsMode};
+use core::num::FpCategory::Subnormal;
 
 const CMD_CREATE: &str = "create";
 const CMD_ADD: &str = "add";
 const CMD_LIST : &str = "list";
 
 fn main() {
-    let cnn_str = "postgres://postgress@localhost:5432";
-    let conn = Connection::connect(cnn_str, TlsMode::None).unwrap();
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .setting(AppSettings::SubcommandRequired)
+        .arg(
+            Arg::with_name("database")
+                .short("d")
+                .long("db")
+                .value_name("ADDR")
+                .help("Sets and address of database connection")
+                .takes_value(true),
+        )
+        .subcommand(SubCommand::with_name(CMD_CREATE).about("crate users table"))
+        .subcommand(SubCommand::with_name(CMD_ADD)).about("add user to the table")
+        .arg(Arg::with_name("NAME")
+            .help("Sets the name of a user")
+            .required(true)
+            .index(1))
+        .arg(Arg::with_name("EMAIL")
+            .help("Sets the email of a user")
+            .required(true)
+            .index(2))
+        .subcommand(SubCommand::with_name(CMD_LIST)).about("print list of users")
+        .get_matches();
+
+    let addr = matches.value_of("database")
+        .unwrap_or("postgres://postgress@locahost:5432");
+
+    let conn = Connection::connect(addr, TlsMode::None).unwrap();
+
 }
 
 fn create_table(conn: &Connection) -> Result<(), Error> {
