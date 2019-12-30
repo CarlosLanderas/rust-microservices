5a1e2ac533062594754bb2a69da64986eca1522a
Carlos Landeras <carlos.landeras@gmail.com>
2019-12-30 09:22:31 +0100

    Add future spawner

--- async/futures-sample/src/main.rs
+++ async/futures-sample/src/main.rs
@@ -1,5 +1,5 @@
 use futures::sync::{oneshot, mpsc};
-use futures::{future, Future, IntoFuture, Stream, Sink};
+use futures::{future, stream, Future, IntoFuture, Stream, Sink};
 
 
 fn to_box<T>(fut: T) -> Box<dyn Future<Item=(), Error=()> + Send>
@@ -53,11 +53,36 @@ fn multiple() {
 
     drop(tx_sink);
     tokio::run(execute_all);
+}
 
+fn send_spawn() {
+    println!("Executing send_span");
+
+    let (tx_sink, rx_stream) = mpsc::channel::<u8>(8);
+    let receiver = rx_stream.fold(0, |acc, value| {
+        println!("Received: {}", value);
+        future::ok(acc + value)
+    }).map(drop);
+
+    let spawner = stream::iter_ok::<_,()>(1u8..11u8)
+        .map(move |x| {
+            let fut = tx_sink.clone().send(x).map(drop).map_err(drop);
+            tokio::spawn(fut);
+        }).collect();
+
+    let execute_all = future::join_all(vec![
+        to_box(spawner),
+        to_box(receiver)
+    ]).map(drop);
+
+    tokio::run(execute_all);
 }
 
 
 fn main() {
     single();
     multiple();
+    send_spawn();
+    println!("Start UDP echo");
+    alt_udp_echo().unwrap();
 }
