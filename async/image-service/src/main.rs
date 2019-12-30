a34dff9f287dfd87f1e37927e13d766d9e3faebc
Carlos Landeras <carlos.landeras@gmail.com>
2019-12-30 10:29:40 +0100

    Implement image download handler

--- async/image-service/src/main.rs
+++ async/image-service/src/main.rs
@@ -7,9 +7,14 @@ use rand::{thread_rng, Rng};
 use rand::distributions::Alphanumeric;
 use std::fs;
 use tokio::fs::File;
-
+use regex::Regex;
+use lazy_static::lazy_static;
+use hyper_staticfile::FileChunkStream;
 
 const INDEX : &str = "<html><body>Images micro service</body></html>";
+lazy_static! {
+    static ref DOWNLOAD_FILE: Regex = Regex::new("^/download/(?P<filename>\\w{20})?$").unwrap();
+}
 
 fn main() {
     let files = Path::new("./files");
@@ -34,6 +39,23 @@ fn microservice_handler(req: Request<Body>, files: &Path) -> Box<Future<Item=Res
     match(req.method(), req.uri().path().to_owned().as_ref()) {
         (&Method::GET, "/") => {
             Box::new(future::ok(Response::new(INDEX.into())))
+        },
+        (&Method::GET, path) if path.starts_with("/download") => {
+            if let Some(cap) = DOWNLOAD_FILE.captures(path) {
+                let filename = cap.name("filename").unwrap().as_str();
+                let mut filepath = files.to_path_buf();
+                filepath.push(filename);
+                let open_file = File::open(filepath);
+                let body = open_file.map(|file| {
+                    let chunks = FileChunkStream::new(file);
+                    Response::new(Body::wrap_stream(chunks))
+                });
+
+                Box::new(body)
+
+            } else {
+                response_with_code(StatusCode::NOT_FOUND)
+            }
         },
         (&Method::POST, "/upload") => {
             let name: String = thread_rng().sample_iter(&Alphanumeric).take(20).collect();
