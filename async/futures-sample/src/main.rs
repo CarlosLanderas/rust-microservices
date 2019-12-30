b1fbfc030ba18d70325c63dabd0d99ec4fdf24fc
Carlos Landeras <carlos.landeras@gmail.com>
2019-12-30 09:38:10 +0100

    Add UDP socket sample

--- async/futures-sample/src/main.rs
+++ async/futures-sample/src/main.rs
@@ -1,6 +1,9 @@
+use std::io;
 use futures::sync::{oneshot, mpsc};
 use futures::{future, stream, Future, IntoFuture, Stream, Sink};
-
+use failure::{Error};
+use tokio::net::{UdpSocket, UdpFramed};
+use tokio::codec::LinesCodec;
 
 fn to_box<T>(fut: T) -> Box<dyn Future<Item=(), Error=()> + Send>
  where
@@ -76,6 +79,38 @@ fn send_spawn() {
     ]).map(drop);
 
     tokio::run(execute_all);
+}
+fn alt_udp_echo() -> Result<(), Error> {
+
+    let from = "0.0.0.0:12345".parse()?;
+    let socket = UdpSocket::bind(&from)?;
+    let framed = UdpFramed::new(socket, LinesCodec::new());
+    let (sink, stream) = framed.split();
+    let (tx, rx) = mpsc::channel(16);
+    let rx = rx.map_err(|_| other("can't take a message"))
+        .fold(sink, |sink, frame| {
+            sink.send(frame)
+        });
+
+    let process = stream.and_then(move |args| {
+        tx.clone()
+            .send(args)
+            .map(drop)
+            .map_err(other)
+    }).collect();
+
+    let execute_all = future::join_all(vec![
+        to_box(rx),
+        to_box(process)
+    ]).map(drop);
+
+    Ok(tokio::run(execute_all))
+}
+
+fn other<E>(err: E) -> io::Error
+where E: Into<Box<std::error::Error + Send + Sync>>
+{
+    io::Error::new(io::ErrorKind::Other, err)
 }
 
 
