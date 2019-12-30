9f074f6cdcd63d976bad2ae55ea0d73627615c11
Carlos <carlos.landeras@gmail.com>
2019-12-30 16:42:32 +0100

    Fix subcommands and sql create script

--- database/postgres/src/main.rs
+++ database/postgres/src/main.rs
@@ -2,13 +2,13 @@ use clap::{
     crate_authors, crate_description, crate_name, crate_version, App, AppSettings, Arg, SubCommand,
 };
 use postgres::{Connection, Error, TlsMode};
-use core::num::FpCategory::Subnormal;
 
 const CMD_CREATE: &str = "create";
 const CMD_ADD: &str = "add";
 const CMD_LIST : &str = "list";
 
-fn main() {
+fn main() -> Result<(), Error> {
+
     let matches = App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
@@ -22,33 +22,54 @@ fn main() {
                 .help("Sets and address of database connection")
                 .takes_value(true),
         )
-        .subcommand(SubCommand::with_name(CMD_CREATE).about("crate users table"))
-        .subcommand(SubCommand::with_name(CMD_ADD)).about("add user to the table")
-        .arg(Arg::with_name("NAME")
-            .help("Sets the name of a user")
-            .required(true)
-            .index(1))
-        .arg(Arg::with_name("EMAIL")
-            .help("Sets the email of a user")
-            .required(true)
-            .index(2))
-        .subcommand(SubCommand::with_name(CMD_LIST)).about("print list of users")
+        .subcommand(SubCommand::with_name(CMD_CREATE).about("create users table"))
+        .subcommand(SubCommand::with_name(CMD_ADD).about("add user to the table")
+                .arg(Arg::with_name("NAME")
+                    .help("Sets the name of a user")
+                    .required(true)
+                    .index(1))
+                .arg(Arg::with_name("EMAIL")
+                    .help("Sets the email of a user")
+                    .required(true)
+                    .index(2)))
+        .subcommand(SubCommand::with_name(CMD_LIST).about("print list of users"))
         .get_matches();
 
     let addr = matches.value_of("database")
-        .unwrap_or("postgres://postgress@locahost:5432");
+        .unwrap_or("postgres://postgres@localhost:5432");
 
     let conn = Connection::connect(addr, TlsMode::None).unwrap();
 
+    match matches.subcommand() {
+        (CMD_CREATE, _) => {
+            create_table(&conn)?;
+        },
+        (CMD_ADD, Some(matches)) => {
+            let name = matches.value_of("NAME").unwrap();
+            let email = matches.value_of("EMAIL").unwrap();
+            create_user(&conn, name, email)?;
+        },
+        (CMD_LIST, _) => {
+            let list = list_users(&conn)?;
+            for (name, email) in list {
+                println!("Name: {:20} - Email {:20}", name, email);
+            }
+        },
+        _ =>  {
+            matches.usage();
+        }
+    }
+
+    Ok(())
 }
 
 fn create_table(conn: &Connection) -> Result<(), Error> {
     conn.execute(
-        "CREATE TABLE USERS (\
-                      id SERIAL PRIMARY KEY
+        "CREATE TABLE users (
+                      id SERIAL PRIMARY KEY,
                       name VARCHAR NOT NULL,
                       email VARCHAR NOT NULL
-   )",
+        )",
         &[],
     )
     .map(drop)
