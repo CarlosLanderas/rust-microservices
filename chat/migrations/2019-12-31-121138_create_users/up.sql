65fc19c66e095da024067f9854f74d0c3bfec994
Carlos Landeras <carlos.landeras@gmail.com>
2019-12-31 13:26:03 +0100

    Fill up and down migration scripts

--- chat/migrations/2019-12-31-121138_create_users/up.sql
+++ chat/migrations/2019-12-31-121138_create_users/up.sql
@@ -1,1 +1,4 @@
--- Your SQL goes here
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE
+);
--- chat/migrations/2019-12-31-121147_create_memberships/up.sql
+++ chat/migrations/2019-12-31-121147_create_memberships/up.sql
@@ -1,1 +1,5 @@
--- Your SQL goes here
+CREATE TABLE memberships (
+ id SERIAL PRIMARY KEY,
+ channel_id INTEGER NOT NULL REFERENCES channels,
+ user_id INTEGER NOT NULL REFERENCES  users
+);
