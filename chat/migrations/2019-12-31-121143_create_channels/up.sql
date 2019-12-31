acedc039b7b9cc6872a06f453c1101cb48f968eb
Carlos Landeras <carlos.landeras@gmail.com>
2019-12-31 14:03:33 +0100

    Modify migration and add chat models

--- chat/migrations/2019-12-31-121143_create_channels/up.sql
+++ chat/migrations/2019-12-31-121143_create_channels/up.sql
@@ -6,3 +6,5 @@ CREATE TABLE channels (
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+SELECT diesel_manage_updated_at('channels');
