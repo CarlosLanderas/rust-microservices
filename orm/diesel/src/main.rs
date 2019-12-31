0d68eef194b7c91176d366116a0710cd1671c0e4
Carlos Landeras <carlos.landeras@gmail.com>
2019-12-31 12:35:55 +0100

    Add insert and load samples

--- orm/diesel/src/main.rs
+++ orm/diesel/src/main.rs
@@ -1,6 +1,48 @@
 #[macro_use]
 extern crate diesel;
+pub mod schema;
+pub mod models;
+use std::env;
+use failure::Error;
+use diesel::r2d2::ConnectionManager;
+use diesel::{SqliteConnection, RunQueryDsl};
+use self::schema::users::dsl::*;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Error> {
+
+    let path = env::var("database").unwrap_or("test.db".to_owned());
+    let manager = ConnectionManager::<SqliteConnection>::new(path);
+    let pool = r2d2::Pool::new(manager)?;
+    let conn = pool.get()?;
+
+    let user1 = models::NewUser {
+        id: &format!("{}", uuid::Uuid::new_v4()),
+        name: "Carlos",
+        email: "carlos@host.com"
+    };
+
+    let user2 = models::NewUser {
+        id: &format!("{}", uuid::Uuid::new_v4()),
+        name: "Geralt",
+        email: "geralt@derivia.com"
+    };
+
+    //Insert users
+    diesel::insert_into(schema::users::table)
+        .values(&user1)
+        .execute(&conn)?;
+
+    diesel::insert_into(schema::users::table)
+        .values(&user2)
+        .execute(&conn)?;
+
+    //List users
+    println!("Listing users...");
+    let mut items = users.load::<models::User>(&conn)?;
+    for user in items {
+        println!("{:?}", user);
+    }
+
+    Ok(())
+
 }
