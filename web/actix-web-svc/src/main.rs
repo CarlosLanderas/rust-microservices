15ed9c3d20b2695feea35420869219f9787c5254
Carlos Landeras <carlos.landeras@gmail.com>
2020-01-01 22:58:53 +0100

    Initial hello world sample

--- web/actix-web-svc/src/main.rs
+++ web/actix-web-svc/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, get, App, HttpResponse, HttpServer, Responder};
+use actix_web::middleware::Logger;
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    HttpServer::new(||
+        App::new()
+            .wrap(Logger::default())
+            .service(index)
+    )
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
+
+#[get("/hello")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
